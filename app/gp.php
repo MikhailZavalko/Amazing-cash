2af40d4a6d5750ad9e7a5bf0af9e70826b829fa7
Mikhail Zavalko <mikhailzavalko@gmail.com>
2020-08-06 15:38:45 +1000

    after upload new presentation and something else))

--- app/gp.php
+++ app/gp.php
@@ -1,50 +1,23 @@
 <?php
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 
+require 'getresponce-api.php';
 
-$name = 'Alexandr';
-$email = '@yandex.ru'; 
+$ip = $_SERVER['REMOTE_ADDR'];
+$getresponse = new GetResponse('su4pwmruj7jv2mtinw4q4citst70kumo');
+$getresponse->enterprise_domain = 'amazing-cash.ru';
+// $getresponse->enterprise_domain = 'localhost';
 
-// $myCurl = curl_init();
-// $isCurlSuccess = curl_setopt_array($myCurl, array(
-//     CURLOPT_URL => 'https://app.getresponse.com/add_subscriber.html',
-//     CURLOPT_RETURNTRANSFER => true,
-//     CURLOPT_POST => true,
-//     CURLOPT_POSTFIELDS => http_build_query(array(
-//     	"email" => $email,
-//     	"name" => $name,
-//     	"start_day" => "0",
-//     	"campaign_token" => "nAwze"
-//     ))
-// ));
-// $response = curl_exec($myCurl);
-// curl_close($myCurl);
+//api URL is relative to your domain UR:
+//$getresponse->api_url = 'https://api3.getresponse360.pl/v3'; //for PL domains
+$getresponse->api_url = 'https://api3.getresponse360.com/v3'; //default
 
 echo '<pre>';
-if (function_exists('curl_init')) {
-echo 'Работает';
-} else {
-echo 'Не работает';
-}
-
-echo $email;
-echo $name;
-echo '</pre>';  
-// echo "Ответ на Ваш запрос: ".$response;
-
-//phpinfo();
-?>
-
-<form action="https://app.getresponse.com/add_subscriber.html" accept-charset="utf-8" method="post"> 
-<!— Поле Email (обязательно) —> 
-email: <input type="text" name="email"/><br/> 
-<!— Токен списка —> 
-<!— Получить API ID на: https://app.getresponse.com/campaign_list.html —> 
-<input type="hidden" name="campaign_token" value="nAwze" /> 
-<!— Добавить подписчика в цикл на определенный день (по желанию) —> 
-<input type="hidden" name="start_day" value="0" /> 
-<!— Кнопка подписаться —> 
-<input type="submit" value="Оставить заявку"/> 
-</form>
+var_dump($getresponse->addContact(array(
+  'name'              => 'ApiTest',
+  'email'             => 'znalex2andr@yandex.ru',
+  'dayOfCycle'        => 0,
+  'campaign'          => array('campaignId' => 'nAwze'),
+  'ipAddress'         => $ip
+  )
+));
+echo '</pre>';
--- app/send.php
+++ app/send.php
@@ -14,14 +14,18 @@ $form_subject = $_POST['form_subject'];
 $name = $_POST['Имя'];
 $number = $_POST['Телефон'];
 $email = $_POST['email'];
+$ip = $_POST['ip'];
 
-
+$message = "";
 
 $myCurl = curl_init();
+//curl_setopt( $myCurl, CURLOPT_HTTPHEADER, array("REMOTE_ADDR: $ip", "HTTP_X_FORWARDED_FOR: $ip"));
 $isCurlSuccess = curl_setopt_array($myCurl, array(
     CURLOPT_URL => 'https://app.getresponse.com/add_subscriber.html',
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POST => true,
+    //CURLOPT_INTERFACE => $ip,
+    //CURLOPT_HTTPHEADER =>  array("REMOTE_ADDR: $ip", "HTTP_X_FORWARDED_FOR: $ip"),
     CURLOPT_POSTFIELDS => http_build_query(array(
     	"email" => $email,
     	"name" => $name,
@@ -32,9 +36,9 @@ $isCurlSuccess = curl_setopt_array($myCurl, array(
 $response = curl_exec($myCurl);
 curl_close($myCurl);
 
-echo $name;
-echo $email;
-echo 'Ответ от сервера'. $response;
+//echo $name;
+//echo $email;
+// echo 'Ответ от сервера'. $response;
 
 // Настройки
 $mail = new PHPMailer;
@@ -47,7 +51,7 @@ $mail->Password = 'qweASD123';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 $mail->setFrom('amazing.cash@yandex.ru');
-$mail->addAddress('amazing.cash@mail.ru');
+//$mail->addAddress('amazing.cash@mail.ru');
 $mail->addAddress('info@amazing-cash.ru');
 
 
@@ -57,7 +61,7 @@ $c = true;
 if ( $method === 'POST' ) {
 
     $project_name = trim($_POST["project_name"]);
-    $admin_email  = trim($_POST["admin_email"]);
+    $admin_email  = trim($_POST["admin_email"] || '');
     $form_subject = trim($_POST["form_subject"]);
 
     foreach ( $_POST as $key => $value ) {
@@ -73,7 +77,7 @@ if ( $method === 'POST' ) {
 } else if ( $method === 'GET' ) {
 
     $project_name = trim($_GET["project_name"]);
-    $admin_email  = trim($_GET["admin_email"]);
+    $admin_email  = trim($_GET["admin_email"] || '');
     $form_subject = trim($_GET["form_subject"]);
 
     foreach ( $_GET as $key => $value ) {
@@ -99,6 +103,7 @@ $mail->Subject = "$form_subject"; // Заголовок письма
 $mail->Body    = "$message"; // Текст письма
 
 
+include "bitrix24.php";
 
 // Результат
 if(!$mail->send()) {
